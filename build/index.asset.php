472406ac6a3fbc47cffcd64d085faf04a368b2ca
Harrison Jude <harrison@hjude.com>
2023-12-14 11:08:03 -0500

    flex layout for height and color of accent bar

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1c083b0da036da8eff04');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e2b89a0125a8cd5d0406');
